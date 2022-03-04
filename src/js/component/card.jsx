52989d37a9fe59acd7182d0be52d296e8cedc13b
Steph123123 <steph13am@gmail.com>
2022-03-04 15:26:02 +0000

    ahi vamos

---
+++ src/js/component/card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Card = () => {
+	return (
+		<div clasName="card">
+			<img src="..." clasName="card-img-top" alt="..." />
+			<div clasName="card-body">
+				<h5 clasName="card-title">Card title</h5>
+				<p clasName="card-text">
+					Some quick example text to build on the card title and make
+					up the bulk of the card's content.
+				</p>
+				<a href="#" clasName="btn btn-primary">
+					Go somewhere
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default Card;
